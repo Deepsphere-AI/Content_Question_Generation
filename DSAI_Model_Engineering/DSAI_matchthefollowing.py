67adc468cdaa353b54efc784d440776eb9a10bca
DeepSphere.AI <61693631+DeepsphereAI@users.noreply.github.com>
2022-10-20 12:39:57 +0530

    Add files via upload
    Updated by Tanya

---
+++ DSAI_Model_Engineering/DSAI_matchthefollowing.py
@@ -0,0 +1,110 @@
+import json
+import requests
+import string
+import re
+import nltk
+import string
+import itertools
+import streamlit as st
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('punkt')
+# import pke
+import yake
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+import traceback
+from flashtext import KeywordProcessor
+import re
+from pprint import pprint
+import random
+import pandas as pd
+# from prettytable import PrettyTable
+# from IPython.display import Markdown, display
+
+def file_selector_match():
+    vAR_file = st.file_uploader('Upload the text file',type=['txt'],key='3')
+    if vAR_file is not None:
+        vAR_text = vAR_file.read().decode("utf-8")
+        st.write('File Content: '+ vAR_text)
+        return vAR_text
+
+# @st.cache(show_spinner=False)
+def get_keywords(text):
+    vAR_out=[]
+    try:
+        
+        
+        kw_yake = yake.KeywordExtractor(top=10,lan="en", n=3, dedupLim=0.3)
+        keywords_yake = kw_yake.extract_keywords(text)
+        for val in keywords_yake:
+            vAR_out.append(val[0])
+            
+        
+    except:
+        vAR_out = []
+        traceback.print_exc()
+    
+
+    return vAR_out
+
+#Extract sentences having the keywords that is extracted before.
+# @st.cache(show_spinner=False)
+def get_sentences_for_keyword(keywords, sentences):
+    vAR_keyword_processor = KeywordProcessor()
+    vAR_keyword_sentences = {}
+    for word in keywords:
+        vAR_keyword_sentences[word] = []
+        vAR_keyword_processor.add_keyword(word)
+    for sentence in sentences:
+        vAR_keywords_found = vAR_keyword_processor.extract_keywords(sentence)
+        for key in vAR_keywords_found:
+            vAR_keyword_sentences[key].append(sentence)
+
+    for key in vAR_keyword_sentences.keys():
+        vAR_values = vAR_keyword_sentences[key]
+        vAR_values = sorted(vAR_values, key=len, reverse=False)
+        vAR_keyword_sentences[key] = vAR_values
+    return vAR_keyword_sentences
+
+
+def sentence_answers(keyword_sentence_mapping):
+    vAR_answers = []
+    vAR_final_sentences = []
+    for k,v in keyword_sentence_mapping.items():
+        if len(v)>0:
+            vAR_match = v[0].lower()
+            vAR_answers.append(k)
+            if k in vAR_match:
+                vAR_temp = re.compile(re.escape(k), re.IGNORECASE)
+                vAR_final_sentences.append(vAR_temp.sub('<answer>',vAR_match))
+            else:
+                vAR_final_sentences.append(vAR_match)
+    return vAR_final_sentences, vAR_answers
+
+# def printmd(string):
+#     display(Markdown(string))
+
+# @st.cache(show_spinner=False)
+# @st.cache(allow_output_mutation=True)
+def question(keyword_sentence_mapping):
+    # tab = PrettyTable()
+    vAR_final_sentences, vAR_answers  = sentence_answers(keyword_sentence_mapping)
+    random.shuffle(vAR_answers)
+    random.shuffle(vAR_final_sentences)
+    vAR_cols_dict = {
+        "A": vAR_answers,
+        "B": vAR_final_sentences
+    }
+    pd.set_option("display.max_colwidth", None)
+    vAR_cols = pd.DataFrame(vAR_cols_dict)
+    return vAR_cols
+    # tab.field_names=['A', 'B']
+    # tab.align["A"] = "l"
+    # tab.align["B"] = "l"
+
+    # # printmd('**Match column A with column B**')
+    # for word,context in zip(answers,final_sentences):
+    #     tab.add_row([word,context.replace("\n"," ")])
+    #     tab.add_row(['',''])
+#     return tab
